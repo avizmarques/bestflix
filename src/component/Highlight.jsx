f7e3b285fb3e3fb65d5f1eb2f418a44a1f63ad95
avizmarques <aviz.marques@gmail.com>
2020-01-21 18:41:03 +0100

    added fetch

--- src/component/Highlight.jsx
+++ src/component/Highlight.jsx
@@ -1,6 +1,18 @@
 import React, { Component } from "react";
 
 export class Highlight extends Component {
+  state = {
+    loading: true
+  };
+
+  componentDidMount() {
+    return fetch(
+      "https://api.themoviedb.org/3/movie/550?api_key=b8bbda4fdff3d12c92a0ff51ac4cf6d9"
+    )
+      .then(res => res.json())
+      .then(info => console.log(info));
+  }
+
   render() {
     return (
       <div>
