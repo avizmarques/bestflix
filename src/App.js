2a0be777bdc60f8518ef87a6c07906c4c1b23519
avizmarques <aviz.marques@gmail.com>
2020-01-21 21:39:27 +0100

    set up for movie item done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import Highlight from './component/Highlight'
-import './App.css';
+import React from "react";
+import Highlight from "./component/Highlight";
+import MovieItem from "./component/MovieItem";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <header className="App-header"></header>
       <Highlight />
-
+      <MovieItem />
     </div>
   );
 }
--- src/component/MovieItem.jsx
+++ src/component/MovieItem.jsx
@@ -1,13 +1,39 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class MovieItem extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
+  state = {
+    loading: true,
+    error: false
+  };
+
+  componentDidMount() {
+    return fetch(
+      "https://api.themoviedb.org/3/discover/movie?api_key=b8bbda4fdff3d12c92a0ff51ac4cf6d9&sort_by=popularity.desc"
+    )
+      .then(res => res.json())
+      .then(info =>
+        this.setState({
+          loading: false,
+          movies: info.results.map(movie => ({
+            title: movie.title,
+            year: movie.release_date,
+            overview: movie.overview,
+            imgUrl: "https://image.tmdb.org/t/p/w500" + movie.poster_path
+          }))
+        })
+      );
+  }
+
+  render() {
+    const { loading, error } = this.state;
+    if (loading) {
+      return <p>Loading...</p>;
+    } else if (error) {
+      return <p>Error!</p>;
+    } else {
+      return <p>Loaded!</p>;
     }
+  }
 }
 
-export default MovieItem
+export default MovieItem;
