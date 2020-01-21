59f9aebcd428cf64ae60636b3f478733905e172d
avizmarques <aviz.marques@gmail.com>
2020-01-21 20:57:03 +0100

    finished set up for highlight component

--- src/component/Highlight.jsx
+++ src/component/Highlight.jsx
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 
 export class Highlight extends Component {
   state = {
-    loading: true
+    loading: true,
+    error: false
   };
 
   componentDidMount() {
@@ -10,20 +11,40 @@ export class Highlight extends Component {
       "https://api.themoviedb.org/3/movie/550?api_key=b8bbda4fdff3d12c92a0ff51ac4cf6d9"
     )
       .then(res => res.json())
-      .then(info => console.log(info));
+      .then(info =>
+        this.setState({
+          loading: false,
+          movie: {
+            title: info.title,
+            year: info.release_date.substring(0, 4),
+            imgUrl: "https://image.tmdb.org/t/p/w500" + info.poster_path,
+            overview: info.overview
+          }
+        })
+      )
+      .catch(error => this.setState({ error: true }));
   }
 
   render() {
-    return (
-      <div>
-        <h1>title</h1>
-        <img />
-        <p>Description</p>
-        <button>favorite</button>
-        <button>upvote</button>
-        <button>downvote</button>
-      </div>
-    );
+    const { loading, error } = this.state;
+    if (loading) {
+      return <h1>Loading</h1>;
+    } else if (error) {
+      return <h1>Error!</h1>;
+    } else {
+      const { title, year, imageUrl, overview } = this.state.movie;
+      return (
+        <div>
+          <h1>{title}</h1>
+          <img src={imageUrl} />
+          <p>{year}</p>
+          <p>{overview}</p>
+          <button>favorite</button>
+          <button>upvote</button>
+          <button>downvote</button>
+        </div>
+      );
+    }
   }
 }
 
