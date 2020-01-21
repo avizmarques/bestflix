796802fb89f93ff978bc9567ae1655c66da4181f
avizmarques <aviz.marques@gmail.com>
2020-01-21 20:18:28 +0100

    added fetch setup

--- src/component/Highlight.jsx
+++ src/component/Highlight.jsx
@@ -5,19 +5,31 @@ export class Highlight extends Component {
     loading: true
   };
 
+  // mapInfo(info) {
+  //   return info.map(info => ({
+  //     title: info.title,
+  //     year: info.release_date,
+  //     imgUrl: "https://image.tmdb.org/t/p/w500/" + info.poster_path
+  //   }));
+  // }
+
   componentDidMount() {
     return fetch(
       "https://api.themoviedb.org/3/movie/550?api_key=b8bbda4fdff3d12c92a0ff51ac4cf6d9"
     )
       .then(res => res.json())
-      .then(info => console.log(info));
+      .then(info => ( { 
+        title: info.title,
+        year: info.release_date,
+        imageUrl: "https://image.tmdb.org/t/p/w500/" + info.poster_path
+      });
   }
 
   render() {
     return (
       <div>
         <h1>title</h1>
-        <img />
+        {/* <img /> */}
         <p>Description</p>
         <button>favorite</button>
         <button>upvote</button>
