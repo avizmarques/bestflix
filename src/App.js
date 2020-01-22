618aa1db223766c955b6777b5615cec4ad82bd4c
avizmarques <aviz.marques@gmail.com>
2020-01-22 17:08:46 +0100

    styles added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <h1>Batflix</h1>
       {/* SearchBar */}
       <Highlight />
       {/* Highlight Should have 1 movie displayed.  */}
