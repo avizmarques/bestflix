054630c18643584f5a5adba3b4818a5031eec96d
avizmarques <aviz.marques@gmail.com>
2020-01-22 15:10:16 +0100

    fixed highlight component

--- src/component/Highlight.jsx
+++ src/component/Highlight.jsx
@@ -2,40 +2,49 @@ import React, { Component } from "react";
 
 export class Highlight extends Component {
   state = {
-    loading: true
+    loading: true,
+    error: false
   };
-
-  // mapInfo(info) {
-  //   return info.map(info => ({
-  //     title: info.title,
-  //     year: info.release_date,
-  //     imgUrl: "https://image.tmdb.org/t/p/w500/" + info.poster_path
-  //   }));
-  // }
 
   componentDidMount() {
     return fetch(
       "https://api.themoviedb.org/3/movie/550?api_key=b8bbda4fdff3d12c92a0ff51ac4cf6d9"
     )
       .then(res => res.json())
-      .then(info => ( { 
-        title: info.title,
-        year: info.release_date,
-        imageUrl: "https://image.tmdb.org/t/p/w500/" + info.poster_path
-      });
+      .then(info =>
+        this.setState({
+          loading: false,
+          movie: {
+            title: info.title,
+            year: info.release_date.substring(0, 4),
+            imgUrl: "https://image.tmdb.org/t/p/w500" + info.poster_path,
+            overview: info.overview
+          }
+        })
+      )
+      .catch(error => this.setState({ error: true }));
   }
 
   render() {
-    return (
-      <div>
-        <h1>title</h1>
-        {/* <img /> */}
-        <p>Description</p>
-        <button>favorite</button>
-        <button>upvote</button>
-        <button>downvote</button>
-      </div>
-    );
+    const { loading, error } = this.state;
+    if (loading) {
+      return <h1>Loading</h1>;
+    } else if (error) {
+      return <h1>Error!</h1>;
+    } else {
+      const { title, year, imgUrl, overview } = this.state.movie;
+      return (
+        <div>
+          <h1>{title}</h1>
+          <img src={imgUrl} alt={title} width="200px" />
+          <p>{year}</p>
+          <p>{overview}</p>
+          <button>favorite</button>
+          <button>upvote</button>
+          <button>downvote</button>
+        </div>
+      );
+    }
   }
 }
 
